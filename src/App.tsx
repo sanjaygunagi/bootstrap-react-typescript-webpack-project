a0be8523339404593045988eddcf2f32c57ae210
Sanjay-SG <sanjay.s.gunagi@gmail.com>
2022-11-06 19:10:23 +0530

    added react refresh - hot reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import "./styles.scss";
 import image from "./react.png";
 import svgImage from "./react-native-1.svg";
+import { Counter } from "./Counter";
 
 export const App = () => {
   return (
     <div>
       <h1>
-        Hello {process.env.NODE_ENV} {process.env.name}
+        Hello App React Typescript Webpack {process.env.NODE_ENV}{" "}
+        {process.env.name}
       </h1>
       <img src={image} alt="Image" width={200} height={200} />
       <img src={svgImage} alt="SVG Image" width={200} height={200} />
+      <Counter />
     </div>
   );
 };
---
+++ src/Counter.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        Count{count}
+      </button>
+    </div>
+  );
+};
