1bcddabe247470dbcc31326d54450d9387c204bb
Sanjay-SG <sanjay.s.gunagi@gmail.com>
2022-11-06 18:56:13 +0530

    webpack config for dev/prod env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import "./styles.scss";
+import image from "./react.png";
+import svgImage from "./react-native-1.svg";
+
 export const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>
+        Hello {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={image} alt="Image" width={200} height={200} />
+      <img src={svgImage} alt="SVG Image" width={200} height={200} />
     </div>
   );
 };
