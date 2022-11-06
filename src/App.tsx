1a030f6148dc6cfaf9cad66e62698e5717c4e076
Sanjay-SG <sanjay.s.gunagi@gmail.com>
2022-11-06 19:47:47 +0530

    prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
-import "./styles.scss";
-import image from "./react.png";
-import svgImage from "./react-native-1.svg";
-import { Counter } from "./Counter";
+import './styles.scss'
+import image from './react.png'
+import svgImage from './react-native-1.svg'
+import { Counter } from './Counter'
 
 export const App = () => {
-  const n = 0;
+  const n = 0
+  const hh = 'Hello'
   return (
     <div>
       <h1>
-        Hello App React Typescript Webpack {process.env.NODE_ENV}{" "}
+        Hello App React Typescript Webpack {process.env.NODE_ENV}{' '}
         {process.env.name}
       </h1>
       <img src={image} alt="Image" width={200} height={200} />
       <img src={svgImage} alt="SVG Image" width={200} height={200} />
       <Counter />
     </div>
-  );
-};
+  )
+}
